39c6461aa7d9ddb6032aeda285327f640cd21473
Wojtek <C:\Users\Wojtek i Marta\Desktop\IT\The BAT!\Gowny katalog poczty programu The Bat!>
2018-07-10 08:16:05 +0200

    add scss styles to components

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -70,18 +70,26 @@ export default class IndecisionApp extends React.Component {
         return (
             <div>
                 <Header title={title} subtitle={subtitle}/>
-                <Action
-                    hasOptions={this.state.options.length > 0}
-                    handlePick={this.handlePick}
-                />
-                <Options
-                    options={this.state.options}
-                    handleDeleteOptions={this.handleDeleteOptions}
-                    handleDeleteOption={this.handleDeleteOption}
-                />
-                <AddOption
-                    handleAddOptionParent={this.handleAddOptionParent}
-                />
+
+                <div className="container">
+                    <Action
+                        hasOptions={this.state.options.length > 0}
+                        handlePick={this.handlePick}
+                    />
+
+                    <div className="widget">
+                        <Options
+                            options={this.state.options}
+                            handleDeleteOptions={this.handleDeleteOptions}
+                            handleDeleteOption={this.handleDeleteOption}
+                        />
+                        <AddOption
+                            handleAddOptionParent={this.handleAddOptionParent}
+                        />
+                    </div>
+
+                </div>
+
                 <OptionModal
                     selectedOption={this.state.selectedOption}
                     handleClearSelectedOption={this.handleClearSelectedOption}
